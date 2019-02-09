db06685dc6ee65d058a5730e9a93872dba2cf29f
Tetiana Blindaruk <t.blindaruk@gmail.com>
2019-02-09 17:00:38 +0200

    added java examples;

---
+++ java-design-pattern/Head-First-Design-Patterns-master/src/headfirst/designpatterns/iterator/dinermergeri/Waitress.java
@@ -0,0 +1,75 @@
+package headfirst.designpatterns.iterator.dinermergeri;
+
+import java.util.Iterator;
+ 
+public class Waitress {
+	Menu pancakeHouseMenu;
+	Menu dinerMenu;
+ 
+	public Waitress(Menu pancakeHouseMenu, Menu dinerMenu) {
+		this.pancakeHouseMenu = pancakeHouseMenu;
+		this.dinerMenu = dinerMenu;
+	}
+ 
+	public void printMenu() {
+		Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
+		Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
+
+		System.out.println("MENU\n----\nBREAKFAST");
+		printMenu(pancakeIterator);
+		System.out.println("\nLUNCH");
+		printMenu(dinerIterator);
+	}
+ 
+	private void printMenu(Iterator<MenuItem> iterator) {
+		while (iterator.hasNext()) {
+			MenuItem menuItem = iterator.next();
+			System.out.print(menuItem.getName() + ", ");
+			System.out.print(menuItem.getPrice() + " -- ");
+			System.out.println(menuItem.getDescription());
+		}
+	}
+ 
+	public void printVegetarianMenu() {
+		System.out.println("\nVEGETARIAN MENU\n----\nBREAKFAST");
+		printVegetarianMenu(pancakeHouseMenu.createIterator());
+		System.out.println("\nLUNCH");
+		printVegetarianMenu(dinerMenu.createIterator());
+	}
+ 
+	public boolean isItemVegetarian(String name) {
+		Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
+		if (isVegetarian(name, pancakeIterator)) {
+			return true;
+		}
+		Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
+		if (isVegetarian(name, dinerIterator)) {
+			return true;
+		}
+		return false;
+	}
+
+
+	private void printVegetarianMenu(Iterator<MenuItem> iterator) {
+		while (iterator.hasNext()) {
+			MenuItem menuItem = iterator.next();
+			if (menuItem.isVegetarian()) {
+				System.out.print(menuItem.getName());
+				System.out.println("\t\t" + menuItem.getPrice());
+				System.out.println("\t" + menuItem.getDescription());
+			}
+		}
+	}
+
+	private boolean isVegetarian(String name, Iterator<MenuItem> iterator) {
+		while (iterator.hasNext()) {
+			MenuItem menuItem = iterator.next();
+			if (menuItem.getName().equals(name)) {
+				if (menuItem.isVegetarian()) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
